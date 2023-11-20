6cdfcb0260de92921bc2953a9001a8b698957eaa
RukiYakutoshi16 <aznmayham@gmail.com>
2023-11-19 20:12:21 -0500

    [Lloyd] fix browse router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import MainPage from "./Project/MainPage";
 import Banner from "./Project/Banner";
-//import { BrowserRouter } from "react-browser-router";
+import { BrowserRouter } from "react-browser-router";
 import TestingAPI from "./Project/TestingAPI";
 
 function App() {
