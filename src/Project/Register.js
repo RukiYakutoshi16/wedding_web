cb1897088581a7f727bbec9ad6dcf8efb5e1b57c
Lloyd <aznmayham@gmail.com>
2023-12-01 15:35:36 -0500

    [Lloyd] add stuff

--- src/Project/Register.js
+++ src/Project/Register.js
@@ -1,20 +1,70 @@
+import { useState } from "react";
+
+let guest = {
+  FirstName: "",
+  LastName: "",
+};
+
+const INSERT = "/InsertUser";
+const GETUSERS = "/GetUsers";
+
 const Register = () => {
+  let [myGuest, setMyGuest] = useState(guest);
+
+  let setFirstName = (e) => {
+    let value = e.target.value;
+    let _guest = {
+      ...myGuest,
+      FirstName: value,
+    };
+    setMyGuest(_guest);
+  };
+
+  let setLastName = (e) => {
+    let value = e.target.value;
+    let _guest = {
+      ...myGuest,
+      LastName: value,
+    };
+    setMyGuest(_guest);
+  };
+
+  let getUsers = async () => {};
+
+  let onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let response = await fetch(INSERT, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          server: "Kestrel",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify(myGuest),
+      });
+    } catch {
+      console.log("can't submit");
+    }
+  };
+
   return (
     <div>
       <h1>Register Here</h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <div>
           {" "}
-          <label>Name</label>
-          <input type="text"></input>
+          <label>First Name</label>
+          <input onChange={setFirstName} type="text"></input>
         </div>
 
         <div>
           {" "}
-          <label>Email</label>
-          <input type="text"></input>
+          <label>Last Name</label>
+          <input onChange={setLastName} type="text"></input>
         </div>
-        <input type="button" value="Submit" />
+        <input onClick={getUsers} type="button" value="Check Regiters" />
+        <input type="submit" value="Submit" />
       </form>
     </div>
   );
